d8120fa1e28abc3886e3cce59d25ad6740459812
ctava <ctava1@yahoo.com>
2016-03-18 14:18:35 -0400

    updated package name

--- opencv/opencv.h
+++ opencv/opencv.h
@@ -12,7 +12,7 @@
 #include <opencv2/photo/photo_c.h>
 #include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/core/core_c.h>
-#include <opencv2/core/objdetect_c.h>
+#include <opencv2/objdetect/objdetect_c.h>
 #include <opencv2/core/version.hpp>
 
 // Trackbar
